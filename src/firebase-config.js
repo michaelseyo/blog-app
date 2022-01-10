967259cf40b9e17097ee325a5f523b5f927b5e65
michael <michael.lcw99@gmail.com>
2022-01-10 22:12:57 +0800

    react init and firebase init

---
+++ src/firebase-config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0PZnpcJGkmo5z_f3Z5YD2t9HRQqx8m_g",
+  authDomain: "web-app-35adf.firebaseapp.com",
+  projectId: "web-app-35adf",
+  storageBucket: "web-app-35adf.appspot.com",
+  messagingSenderId: "1009694409427",
+  appId: "1:1009694409427:web:a50bf97509dbf19b2d4a26",
+  measurementId: "G-R3ZXNQY4SZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider();
