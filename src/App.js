18483da86f9736ff1df9b20f60f113c446f4d938
michael <michael.lcw99@gmail.com>
2022-01-10 23:11:06 +0800

    react router setup and navbar style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+
+import { AppBar, Toolbar, Button } from '@material-ui/core';
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <AppBar color="primary">
+        <Toolbar>
+          <Button>
+            <Link to="/">Home</Link>
+          </Button>
+          <Button>
+            <Link to="/createpost">Create</Link>
+          </Button>
+          <Button>
+            <Link to="/login">Login</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/createpost" element={<CreatePost />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
