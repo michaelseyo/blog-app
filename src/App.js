676f3cdf9daf1559d41c3d73c457b35870bb70b7
michael <michael.lcw99@gmail.com>
2022-01-11 15:21:33 +0800

    created auth and navbar functions

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
+import { auth } from "./firebase-config";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
 
 import { AppBar, Toolbar, Button } from '@material-ui/core';
 
 function App() {
+  const [isAuth, setAuth] = useState(false);
+  const logUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setAuth(false);
+      window.location.pathname = "/login";
+    });
+  }
+
   return (
     <Router>
-      <AppBar color="primary">
+      <AppBar color="primary" position="sticky">
         <Toolbar>
           <Button>
             <Link to="/">Home</Link>
@@ -17,15 +29,21 @@ function App() {
           <Button>
             <Link to="/createpost">Create</Link>
           </Button>
-          <Button>
-            <Link to="/login">Login</Link>
-          </Button>
+          {!isAuth ? 
+            <Button>
+              <Link to="/login">Login</Link> 
+            </Button> : 
+            <Button onClick={logUserOut}
+              style={{
+                color:"white"           
+              }}>Log Out</Button>
+          }
         </Toolbar>
       </AppBar>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/createpost" element={<CreatePost />}/>
-        <Route path="/login" element={<Login />}/>
+        <Route path="/login" element={<Login setAuth={setAuth}/>}/>
       </Routes>
     </Router>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { auth, provider } from '../firebase-config';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-    return <div> Login </div>
+function Login({setAuth}) {
+    let navigate = useNavigate();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then(() => {
+            localStorage.setItem("isAuth", true);
+            setAuth(true);
+            navigate("/");
+        });
+    }
+    return (
+        <div> 
+            <p>Sign in with Google to continue</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+    );
 }
 
 export default Login;
